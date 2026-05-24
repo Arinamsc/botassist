10ebcd48a414e2db5e6f5d51e146f8aa60c0fbcb
Arinamsc <as@pragmatica.design>
2026-05-24 21:44:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "ВСТАВЬ_ТОКЕН_СЮДА")
+
+import os
+
+BOT_TOKEN = "8202443073:AAHKP_1OiFbywPuoe7ZJggvMy4qzQsQKCD8"
+ANTHROPIC_API_KEY = "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"
+
